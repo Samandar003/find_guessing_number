b8598aeb43666def86549b71f6b1c5d2d0e99628
Samandar003 <samandar200527@gmail.com>
2022-03-03 14:14:56 +0500

    pc_guess

--- find_number.py
+++ find_number.py
@@ -1,1 +1,22 @@
-number = print()
+import random as r
+
+pc_guess = r.randint(1, 10)
+print('1 dan 10 gacha son o\'yladim topolasizma? ')
+
+taxminlar = 0
+while True:
+  taxminlar += 1
+  user_guess = int(input('>>>> '))
+  if pc_guess > user_guess:
+    print(user_guess, ' dan katta yana urining')
+    continue
+  elif pc_guess < user_guess:
+    print(user_guess, ' dan kichik  yana urining')
+    continue
+  else:
+    print('topdingiz')
+    break
+print(taxminlar)
+
+
+
